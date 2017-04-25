8aca2312d4ae3a87b42db01df3a861af2d48e4b3
Tiffany Tran <tiffany7@vt.edu>
2017-04-25 13:56:28 -0400

    fixed value as integer problem. remove modified field from your db table

--- src/Model/Entity/Service.php
+++ src/Model/Entity/Service.php
@@ -17,7 +17,6 @@ use Cake\ORM\Entity;
  * @property string $image_url
  * @property float $min_price
  * @property \Cake\I18n\Time $created
- * @property \Cake\I18n\Time $modified
  *
  * @property \App\Model\Entity\User $user
  */
